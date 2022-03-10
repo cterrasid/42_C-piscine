c1ac7a1d33c97cd1a9f256ad19e6e37fa3561b12
Clarette Terrasi Diaz <clarette.terrasi@gmail.com>
2022-03-10 01:37:36 +0100

    Add ex07

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void    ft_putnbr(int nb)
+{
+    if (nb == -2147483648)
+    {
+        write(1, "-2147483648", 11);
+        return ;
+    }
+    else if (nb < 0)
+    {
+        ft_putchar('-');
+        ft_putnbr(nb * -1);
+    }
+    else if (nb > 9)
+    {
+        ft_putnbr(nb / 10);
+        ft_putnbr(nb % 10);
+    }
+    else
+    {
+        ft_putchar(nb + '0');
+    }
+}
+
+int main(void)
+{
+    ft_putnbr(3);
+    return (0);
+}
